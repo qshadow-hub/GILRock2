112e7a7ba220251a243f6dbb76316d4939b16e53
qshadow-hub <qshadowqshadow@gmail.com>
2025-12-15 21:36:46 +0200

    Update GILRock.lua

--- GILRock.lua
+++ GILRock.lua
@@ -1,40 +1,168 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "Rayfield Example Window",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "Rayfield Interface Suite",
-   LoadingSubtitle = "by Sirius",
-   ShowText = "Rayfield", -- for mobile users to unhide rayfield, change if you'd like
-   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
-
-   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
-
+   Name = "GILRock",
+   Icon = 0,
+   LoadingTitle = "GILRock",
+   LoadingSubtitle = "qShadow",
+   ShowText = "Rayfield",
+   Theme = "Default",
+   ToggleUIKeybind = "K",
    DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
-
+   DisableBuildWarnings = false,
    ConfigurationSaving = {
       Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
+      FolderName = nil,
       FileName = "Big Hub"
    },
-
    Discord = {
-      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+      Enabled = false,
+      Invite = "noinvitelink",
+      RememberJoins = true
    },
-
-   KeySystem = false, -- Set this to true to use our key system
+   KeySystem = true,
    KeySettings = {
-      Title = "Untitled",
+      Title = "GILRock Key",
       Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+      Note = "No method of obtaining the key is provided",
+      FileName = "Key",
+      SaveKey = true,
+      GrabKeyFromSite = false,
+      Key = {"Hello"}
    }
 })
 
-local Tab = Window:CreateTab("Autos", 4483362458) -- Title, Image
+-- ========== AUTOS TAB ==========
+local AutosTab = Window:CreateTab("Autos", nil)
+local AutosSection = AutosTab:CreateSection("Autos")
+
+local Dropdown1 = AutosTab:CreateDropdown({
+   Name = "Dropdown Example",
+   Options = {"Option 1","Option 2"},
+   CurrentOption = "Option 1",
+   MultipleOptions = false,
+   Flag = "Dropdown1",
+   Callback = function(Option)
+       print("Selected option:", Option)
+   end,
+})
+
+local Toggle1 = AutosTab:CreateToggle({
+   Name = "Temp1",
+   CurrentValue = false,
+   Flag = "Toggle1",
+   Callback = function(Value)
+       print("Temp1:", Value)
+   end,
+})
+
+local Toggle2 = AutosTab:CreateToggle({
+   Name = "Temp2",
+   CurrentValue = false,
+   Flag = "Toggle2",
+   Callback = function(Value)
+       print("Temp2:", Value)
+   end,
+})
+
+local Toggle3 = AutosTab:CreateToggle({
+   Name = "Temp3",
+   CurrentValue = false,
+   Flag = "Toggle3",
+   Callback = function(Value)
+       print("Temp3:", Value)
+   end,
+})
+
+-- ========== CHARACTER TAB ==========
+local CharacterTab = Window:CreateTab("Character", nil)
+local CharacterSection = CharacterTab:CreateSection("Character")
+
+local CharToggle1 = CharacterTab:CreateToggle({
+   Name = "Temp1",
+   CurrentValue = false,
+   Flag = "CharToggle1",
+   Callback = function(Value)
+       print("Character Temp1:", Value)
+   end,
+})
+
+local CharToggle2 = CharacterTab:CreateToggle({
+   Name = "Temp2",
+   CurrentValue = false,
+   Flag = "CharToggle2",
+   Callback = function(Value)
+       print("Character Temp2:", Value)
+   end,
+})
+
+local CharToggle3 = CharacterTab:CreateToggle({
+   Name = "Temp3",
+   CurrentValue = false,
+   Flag = "CharToggle3",
+   Callback = function(Value)
+       print("Character Temp3:", Value)
+   end,
+})
+
+-- ========== ESP TAB ==========
+local ESPTab = Window:CreateTab("ESP", nil)
+local ESPSection = ESPTab:CreateSection("ESP")
+
+local ESPToggle1 = ESPTab:CreateToggle({
+   Name = "Temp1",
+   CurrentValue = false,
+   Flag = "ESPToggle1",
+   Callback = function(Value)
+       print("ESP Temp1:", Value)
+   end,
+})
+
+local ESPToggle2 = ESPTab:CreateToggle({
+   Name = "Temp2",
+   CurrentValue = false,
+   Flag = "ESPToggle2",
+   Callback = function(Value)
+       print("ESP Temp2:", Value)
+   end,
+})
+
+local ESPToggle3 = ESPTab:CreateToggle({
+   Name = "Temp3",
+   CurrentValue = false,
+   Flag = "ESPToggle3",
+   Callback = function(Value)
+       print("ESP Temp3:", Value)
+   end,
+})
+
+-- ========== TELEPORT TAB ==========
+local TeleportTab = Window:CreateTab("Teleport", nil)
+local TeleportSection = TeleportTab:CreateSection("Teleport")
+
+local TeleToggle1 = TeleportTab:CreateToggle({
+   Name = "Temp1",
+   CurrentValue = false,
+   Flag = "TeleToggle1",
+   Callback = function(Value)
+       print("Teleport Temp1:", Value)
+   end,
+})
+
+local TeleToggle2 = TeleportTab:CreateToggle({
+   Name = "Temp2",
+   CurrentValue = false,
+   Flag = "TeleToggle2",
+   Callback = function(Value)
+       print("Teleport Temp2:", Value)
+   end,
+})
+
+local TeleToggle3 = TeleportTab:CreateToggle({
+   Name = "Temp3",
+   CurrentValue = false,
+   Flag = "TeleToggle3",
+   Callback = function(Value)
+       print("Teleport Temp3:", Value)
+   end,
+})
