6fe0106df532664cd389f7e48db54513215194b7
qshadow-hub <qshadowqshadow@gmail.com>
2025-12-15 22:17:48 +0200

    Update GILRock.lua

--- GILRock.lua
+++ GILRock.lua
@@ -87,27 +87,118 @@ local CharToggle1 = CharacterTab:CreateToggle({
    end,
 })
 
+local walkSpeedEnabled = false
+local jumpPowerEnabled = false
+local walkSpeedValue = 16
+local jumpPowerValue = 50
+local walkSpeedConnection = nil
+local jumpPowerConnection = nil
+
+local function applyWalkSpeed()
+    local player = game.Players.LocalPlayer
+    
+    if walkSpeedConnection then
+        walkSpeedConnection:Disconnect()
+    end
+    
+    local function setSpeed()
+        if player.Character and player.Character:FindFirstChild("Humanoid") then
+            player.Character.Humanoid.WalkSpeed = walkSpeedValue
+        end
+    end
+    
+    walkSpeedConnection = game:GetService("RunService").Heartbeat:Connect(function()
+        if walkSpeedEnabled then
+            setSpeed()
+        end
+    end)
+end
+
+local function applyJumpPower()
+    local player = game.Players.LocalPlayer
+    
+    if jumpPowerConnection then
+        jumpPowerConnection:Disconnect()
+    end
+    
+    local function setJump()
+        if player.Character and player.Character:FindFirstChild("Humanoid") then
+            local humanoid = player.Character.Humanoid
+            if humanoid.UseJumpPower then
+                humanoid.JumpPower = jumpPowerValue
+            else
+                humanoid.JumpHeight = jumpPowerValue / 10
+            end
+        end
+    end
+    
+    jumpPowerConnection = game:GetService("RunService").Heartbeat:Connect(function()
+        if jumpPowerEnabled then
+            setJump()
+        end
+    end)
+end
+
+-- Character respawn handler
+game.Players.LocalPlayer.CharacterAdded:Connect(function()
+    task.wait(0.5)
+    if walkSpeedEnabled then
+        applyWalkSpeed()
+    end
+    if jumpPowerEnabled then
+        applyJumpPower()
+    end
+end)
+
+local WalkSpeedToggle = CharacterTab:CreateToggle({
+   Name = "Enable WalkSpeed",
+   CurrentValue = false,
+   Flag = "WalkSpeedToggle",
+   Callback = function(Value)
+       walkSpeedEnabled = Value
+       if Value then
+           applyWalkSpeed()
+       elseif walkSpeedConnection then
+           walkSpeedConnection:Disconnect()
+       end
+   end,
+})
+
 local Slider1 = CharacterTab:CreateSlider({
    Name = "WalkSpeed",
-   Range = {0, 1000},
+   Range = {0, 500},
    Increment = 10,
-   Suffix = "WalkSpeed",
-   CurrentValue = 10,
+   Suffix = "Speed",
+   CurrentValue = 16,
    Flag = "Slider1",
    Callback = function(Value)
-       print("Slider value:", Value)
+       walkSpeedValue = Value
+   end,
+})
+
+local JumpPowerToggle = CharacterTab:CreateToggle({
+   Name = "Enable JumpPower",
+   CurrentValue = false,
+   Flag = "JumpPowerToggle",
+   Callback = function(Value)
+       jumpPowerEnabled = Value
+       if Value then
+           applyJumpPower()
+       elseif jumpPowerConnection then
+           jumpPowerConnection:Disconnect()
+       end
    end,
 })
 
 local Slider2 = CharacterTab:CreateSlider({
    Name = "JumpPower",
-   Range = {0, 1000},
+   Range = {0, 500},
    Increment = 10,
-   Suffix = "JumpPower",
-   CurrentValue = 10,
+   Suffix = "Power",
+   CurrentValue = 50,
    Flag = "Slider2",
    Callback = function(Value)
-       print("Slider value:", Value)
+       jumpPowerValue = Value
    end,
 })
 
@@ -117,7 +208,8 @@ local ESPSection = ESPTab:CreateSection("ESP")
 
 -- Player Dropdown for ESP
 local Players = game:GetService("Players")
-local selectedPlayer = nil
+local selectedPlayerESP = nil
+local PlayerDropdownESP
 
 local function getPlayerNames()
     local names = {}
@@ -127,50 +219,150 @@ local function getPlayerNames()
     return names
 end
 
-local PlayerDropdown = ESPTab:CreateDropdown({
+PlayerDropdownESP = ESPTab:CreateDropdown({
    Name = "Select Player",
    Options = getPlayerNames(),
-   CurrentOption = "None",
+   CurrentOption = {"None"},
    MultipleOptions = false,
-   Flag = "PlayerDropdown",
+   Flag = "PlayerDropdownESP",
    Callback = function(Option)
-       selectedPlayer = Players:FindFirstChild(Option)
-       if selectedPlayer then
-           print("Selected player:", selectedPlayer.Name)
+       selectedPlayerESP = Players:FindFirstChild(Option)
+       if selectedPlayerESP then
+           print("ESP Selected player:", selectedPlayerESP.Name)
        end
    end,
 })
 
--- Update dropdown when players join or leave
-Players.PlayerAdded:Connect(function()
-    PlayerDropdown:Refresh(getPlayerNames())
+-- Create a button to manually refresh player list
+local RefreshButtonESP = ESPTab:CreateButton({
+   Name = "Refresh Player List",
+   Callback = function()
+       Rayfield:Notify({
+           Title = "Players Updated",
+           Content = "Player list has been refreshed!",
+           Duration = 2,
+           Image = 4483362458,
+       })
+       -- Update the dropdown options
+       PlayerDropdownESP:Set(getPlayerNames())
+   end,
+})
+
+local OresDropdownESP = ESPTab:CreateDropdown({
+   Name = "Select Ore",
+   Options = {"Stone","Basalt","LimeStone","Iron","Gold","Diamond","Emerald"},
+   CurrentOption = {"Stone"},
+   MultipleOptions = false,
+   Flag = "OresDropdownESP",
+   Callback = function(Option)
+       print("ESP Selected ore:", Option)
+   end,
+})
+
+local NpcDropdownESP = ESPTab:CreateDropdown({
+   Name = "Select NPC",
+   Options = {"Merchant","Guard","Trader","Boss","Enemy"},
+   CurrentOption = {"Merchant"},
+   MultipleOptions = false,
+   Flag = "NpcDropdownESP",
+   Callback = function(Option)
+       print("ESP Selected NPC:", Option)
+   end,
+})
+
+-- Auto refresh on player join/leave
+Players.PlayerAdded:Connect(function(plr)
+    task.wait(0.5)
+    Rayfield:Notify({
+        Title = "Player Joined",
+        Content = plr.Name .. " joined the game!",
+        Duration = 2,
+        Image = 4483362458,
+    })
 end)
 
-Players.PlayerRemoving:Connect(function()
-    PlayerDropdown:Refresh(getPlayerNames())
+Players.PlayerRemoving:Connect(function(plr)
+    Rayfield:Notify({
+        Title = "Player Left",
+        Content = plr.Name .. " left the game!",
+        Duration = 2,
+        Image = 4483362458,
+    })
 end)
 
 local ESPToggle2 = ESPTab:CreateToggle({
-   Name = "Ores",
+   Name = "Enable Ores ESP",
    CurrentValue = false,
    Flag = "ESPToggle2",
    Callback = function(Value)
-       print("ESP Temp2:", Value)
+       print("ESP Ores Toggle:", Value)
    end,
 })
 
 local ESPToggle3 = ESPTab:CreateToggle({
-   Name = "Npc",
+   Name = "Enable NPC ESP",
    CurrentValue = false,
    Flag = "ESPToggle3",
    Callback = function(Value)
-       print("ESP Temp3:", Value)
+       print("ESP NPC Toggle:", Value)
    end,
 })
 
 -- ========== TELEPORT TAB ==========
 local TeleportTab = Window:CreateTab("Teleport", nil)
 local TeleportSection = TeleportTab:CreateSection("Teleport")
+
+local selectedPlayerTele = nil
+local PlayerDropdownTele
+
+PlayerDropdownTele = TeleportTab:CreateDropdown({
+   Name = "Select Player",
+   Options = getPlayerNames(),
+   CurrentOption = {"None"},
+   MultipleOptions = false,
+   Flag = "PlayerDropdownTele",
+   Callback = function(Option)
+       selectedPlayerTele = Players:FindFirstChild(Option)
+       if selectedPlayerTele then
+           print("Teleport Selected player:", selectedPlayerTele.Name)
+       end
+   end,
+})
+
+local RefreshButtonTele = TeleportTab:CreateButton({
+   Name = "Refresh Player List",
+   Callback = function()
+       Rayfield:Notify({
+           Title = "Players Updated",
+           Content = "Player list has been refreshed!",
+           Duration = 2,
+           Image = 4483362458,
+       })
+       PlayerDropdownTele:Set(getPlayerNames())
+   end,
+})
+
+local OresDropdownTele = TeleportTab:CreateDropdown({
+   Name = "Select Ore",
+   Options = {"Stone","Basalt","LimeStone","Iron","Gold","Diamond","Emerald"},
+   CurrentOption = {"Stone"},
+   MultipleOptions = false,
+   Flag = "OresDropdownTele",
+   Callback = function(Option)
+       print("Teleport Selected ore:", Option)
+   end,
+})
+
+local NpcDropdownTele = TeleportTab:CreateDropdown({
+   Name = "Select NPC",
+   Options = {"Merchant","Guard","Trader","Boss","Enemy"},
+   CurrentOption = {"Merchant"},
+   MultipleOptions = false,
+   Flag = "NpcDropdownTele",
+   Callback = function(Option)
+       print("Teleport Selected NPC:", Option)
+   end,
+})
 
 local TeleToggle1 = TeleportTab:CreateToggle({
    Name = "Players",
